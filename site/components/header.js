32977624c4cbc03a92871700e5b8edd3d8d22981
Net_ <the.net.persists@gmail.com>
2023-01-05 20:17:54 +0000

    Header custom element, started tutorials.

---
+++ site/components/header.js
@@ -0,0 +1,77 @@
+const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+  <style>
+    * { margin: 0; padding: 0; box-sizing: border-box; }
+
+    header {
+      box-sizing: border-box;
+      padding: 1.5vh 5vw 0 5vw;
+      position: relative;
+      width: 100%;
+    }
+
+    nav {
+      margin: 0 auto;
+    }
+
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      gap: 1.5vw;
+      list-style: none;
+      margin: 0 auto;
+    }
+
+    a {
+      text-decoration: none;
+      color: #fff;
+      display: block;
+    }
+
+    strong {
+      border-bottom: 1px solid;
+      display: block;
+    }
+
+    @media only screen and (max-width: 680px) {
+      ul {
+        justify-content: space-between;
+        width: 100%;
+      }
+
+      li { font-size: 16px; }
+    }
+  </style>
+  <header>
+    <nav>
+      <ul>
+        <li><a href="/home.html" id="home">Home</a>
+        <li><a href="/worlds.html" id="worlds">Worlds</a>
+        <li><a href="/tutorials.html" id="tutorials">Tutorials</a>
+        <li><a href="/roadmap.html" id="roadmap">Roadmap</a>
+        <li><a href="/blog.html" id="blog">Blog</a>
+      </ul>
+    </nav>
+  </header>
+`;
+
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+    shadowRoot.appendChild(headerTemplate.content);
+
+    // Wrap the selected link in <strong>.
+    var selectedId = this.attributes.selected.value;
+    var elementHtml = shadowRoot.getElementById(selectedId).innerHTML;
+    var newHtml = "<strong>" + elementHtml + "</strong>";
+    shadowRoot.getElementById(selectedId).innerHTML = newHtml;
+  }
+}
+
+customElements.define('header-component', Header);
